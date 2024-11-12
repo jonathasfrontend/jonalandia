f1bf14ff399e7e1be798eda42f0dc5126cfac518
Jonathas <jonathass5678@gmail.com>
2024-11-12 17:42:27 -0300

    jonalandia_bot

--- src/config/bdServerConect.js
+++ src/config/bdServerConect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { info, erro } = require('../logger');
 
 function bdServerConect() {
-    mongoose.connect("seu link de conexão com o MongoDB")
+    mongoose.connect("sua string de conexão")
         .then(() => info.info('Conectado ao MongoDB'))
         .catch(err => erro.error('Erro ao conectar ao MongoDB', err));
 }
