f50487c5551c034edb2dcad8f09ac50a8137673e
Jonathas <jonathass5678@gmail.com>
2024-11-19 10:00:13 -0300

    jonalandia

--- src/api.js
+++ src/api.js
@@ -5,7 +5,6 @@ function getApiUrl() {
     const api = axios.create({
         baseURL: process.env.URL_CONECTION_API_MODERATION_USER,
     });
-
     return api;
 }
 
@@ -13,21 +12,16 @@ async function testApiConnection() {
     const api = getApiUrl();
 
     try {
-        // Testa a conexão com uma chamada simples à API
         const response = await api.get('/');
         info.info('Conexão com a API bem-sucedida:', response.statusText);
     } catch (error) {
-        // Verifica o tipo de erro e exibe uma mensagem apropriada
         if (error.code === 'ECONNABORTED') {
             erro.error('Erro: Tempo de conexão excedido. Verifique a disponibilidade da API.');
         } else if (error.response) {
-            // Erros relacionados à resposta HTTP (4xx, 5xx)
             erro.error(`Erro na API: ${error.response.status} - ${error.response.statusText}`);
         } else if (error.request) {
-            // Erros relacionados à ausência de resposta
             erro.error('Erro: A API não respondeu. Verifique sua conexão com a internet ou a URL da API.');
         } else {
-            // Outros erros
             erro.error('Erro inesperado ao conectar com a API:', error.message);
         }
     }
