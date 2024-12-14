0ea439691c38ca1760e4774fcefa537c7d1bcd0a
Jonathas <jonathass5678@gmail.com>
2024-12-14 14:24:47 -0300

    Jonalandia_bot

--- src/commands/transferir.js
+++ src/commands/transferir.js
@@ -17,18 +17,9 @@ async function tranferirMoeda(interaction) {
     }
 
     try {
-        // Busca os dados do remetente e do destinatário
         const senderData = await User.findOne({ username: sender.username });
         const recipientData = await User.findOne({ username: recipient.username });
 
-        console.log('Saldo do remetente:', senderData.coins);
-        console.log('Quantidade solicitada:', amount);
-        console.log('Saldo do destinatário:', recipientData.coins);
-        console.log('Remetente:', senderData);
-        console.log('Destinatário:', recipientData);
-
-
-        // Verifica se o remetente possui um perfil registrado
         if (!senderData) {
             return interaction.reply({
                 content: 'Você ainda não possui um perfil registrado.',
@@ -36,7 +27,6 @@ async function tranferirMoeda(interaction) {
             });
         }
 
-        // Verifica se o destinatário possui um perfil registrado
         if (!recipientData) {
             return interaction.reply({
                 content: 'O destinatário ainda não possui um perfil registrado.',
@@ -44,7 +34,6 @@ async function tranferirMoeda(interaction) {
             });
         }
 
-        // Verifica se o remetente tem moedas suficientes
         if (senderData.coins < amount) {
             return interaction.reply({
                 content: `Você não tem moedas suficientes para realizar essa transferência. Seu saldo atual é ${senderData.coins} moedas.`,
@@ -52,14 +41,12 @@ async function tranferirMoeda(interaction) {
             });
         }
 
-        // Atualiza os saldos no banco de dados
         senderData.coins -= amount;
         recipientData.coins += amount;
 
         await senderData.save();
         await recipientData.save();
 
-        // Criação do Embed de confirmação
         const embed = new EmbedBuilder()
             .setColor('Green')
             .setTitle('Transferência Concluída')
@@ -72,7 +59,6 @@ async function tranferirMoeda(interaction) {
 
         await interaction.reply({ embeds: [embed] });
 
-        // Logs
         info.info(`Transferência de ${amount} moedas de ${sender.tag} para ${recipient.tag}.`);
     } catch (error) {
         erro.error('Erro ao executar o comando de transferência', error);
